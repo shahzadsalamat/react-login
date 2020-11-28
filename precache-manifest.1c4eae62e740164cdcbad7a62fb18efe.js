87ad6f1fcc1fe4eecdfd600380050599bb8d5294
shahzad salamat <shahzad.salamat@yahoo.com>
2020-11-29 00:23:28 +0200

    Updates

---
+++ precache-manifest.1c4eae62e740164cdcbad7a62fb18efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de6ba9266dde25c8da3b75a2d09cec0",
+    "url": "/react-login/index.html"
+  },
+  {
+    "revision": "a80a2556b36153fc5c22",
+    "url": "/react-login/static/css/main.e9aa3b30.chunk.css"
+  },
+  {
+    "revision": "447e2cd949eb4fb6dd1f",
+    "url": "/react-login/static/js/2.d89aa215.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/react-login/static/js/2.d89aa215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80a2556b36153fc5c22",
+    "url": "/react-login/static/js/main.609abd8f.chunk.js"
+  },
+  {
+    "revision": "14c4d8b88bbb55cca241",
+    "url": "/react-login/static/js/runtime-main.965954af.js"
+  }
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ServiceProvider from './serviceProvider/serviceProvider.js';
-
-ReactDOM.render(<ServiceProvider />, document.getElementById('root'));
